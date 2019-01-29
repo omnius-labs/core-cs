d2f48f7622d7ac193a590c50627551af21e77753
Lyrise <lyrise1984@gmail.com>
2019-01-29 12:17:02 +0900

    Fix TaskManagerクラスの動作を再利用不可になるよう変更した

--- src/Omnix.Base/TaskManager.cs
+++ src/Omnix.Base/TaskManager.cs
@@ -7,14 +7,12 @@ namespace Omnix.Base
     /// <summary>
     /// <see cref="Task"/>の管理機能を提供します。
     /// </summary>
-    public sealed class TaskManager : ServiceBase
+    public sealed class TaskManager : DisposableBase
     {
         private readonly Action<CancellationToken> _callback;
 
         private Task _task;
         private CancellationTokenSource _tokenSource;
-
-        private ServiceStateType _state = ServiceStateType.Stopped;
 
         private readonly AsyncLock _asyncLock = new AsyncLock();
         private volatile bool _disposed;
@@ -24,94 +22,21 @@ namespace Omnix.Base
             _callback = callback;
         }
 
-        public void Wait()
-        {
-            _task?.Wait();
-        }
+        public Task Task => _task;
 
-        public bool Wait(int millisecondsTimeout)
+        public void Start()
         {
-            return _task?.Wait(millisecondsTimeout) ?? true;
-        }
-
-        public bool Wait(int millisecondsTimeout, CancellationToken cancellationToken)
-        {
-            return _task?.Wait(millisecondsTimeout, cancellationToken) ?? true;
-        }
-
-        public void Wait(CancellationToken cancellationToken)
-        {
-            _task?.Wait(cancellationToken);
-        }
-
-        public bool Wait(TimeSpan timeout)
-        {
-            return _task?.Wait(timeout) ?? true;
-        }
-
-        public override ServiceStateType StateType
-        {
-            get
-            {
-                return _state;
-            }
-        }
-
-        internal void InternalStart()
-        {
-            if (this.StateType != ServiceStateType.Stopped) return;
-            _state = ServiceStateType.Starting;
-
             _tokenSource = new CancellationTokenSource();
             _task = new Task(() =>
             {
                 _callback(_tokenSource.Token);
             }, _tokenSource.Token, TaskCreationOptions.LongRunning);
             _task.Start();
-
-            _state = ServiceStateType.Running;
         }
 
-        internal void InternalStop()
+        public void Cancel()
         {
-            if (this.StateType != ServiceStateType.Running) return;
-            _state = ServiceStateType.Stopping;
-
             _tokenSource.Cancel();
-            _task.Wait();
-
-            _task.Dispose();
-            _task = null;
-
-            _tokenSource.Dispose();
-            _tokenSource = null;
-
-            _state = ServiceStateType.Stopped;
-        }
-
-        public override async ValueTask Start(CancellationToken token = default)
-        {
-            using (await _asyncLock.LockAsync())
-            {
-                this.InternalStart();
-            }
-        }
-
-        public override async ValueTask Stop(CancellationToken token = default)
-        {
-            using (await _asyncLock.LockAsync())
-            {
-                this.InternalStop();
-            }
-        }
-
-        public override async ValueTask Restart(CancellationToken token = default)
-        {
-            using (await _asyncLock.LockAsync())
-            {
-                this.InternalStop();
-                this.InternalStart();
-            }
         }
 
         protected override void Dispose(bool disposing)
