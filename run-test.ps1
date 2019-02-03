1efdec4230186a45464883abbbce2e9519ca05e4
Lyrise <lyrise1984@gmail.com>
2019-02-03 21:25:57 +0900

    Fix .gitlab-ci.ymlを削除し、自動テストを止める修正を行った

--- run-test.ps1
+++ run-test.ps1
@@ -1,9 +1,9 @@
 dotnet tool install --global coverlet.console
-dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
+dotnet tool install --global dotnet-reportgenerator-globaltool
 
 ForEach ($folder in (Get-ChildItem -Path "tests" -Directory)) 
 {
     dotnet test $folder.FullName /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="../TestResults/$folder-opencover.xml";
 }
 
-tools\reportgenerator.exe "--reports:tests/TestResults/*-opencover.xml" "--targetdir:tests/TestResults/html"
+reportgenerator.exe "--reports:tests/TestResults/*-opencover.xml" "--targetdir:tests/TestResults/html"
--- run-test.sh
+++ run-test.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y --no-install-recommends libc6-dev
 
 dotnet tool install --global coverlet.console
-dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
+dotnet tool install --global dotnet-reportgenerator-globaltool
 
 for path in `find "tests" -maxdepth 2 -type f -name "*.csproj"`
 do
@@ -12,4 +12,4 @@ do
     dotnet test "$path" -p:CollectCoverage=true -p:CoverletOutputFormat=opencover -p:CoverletOutput=$output;
 done
 
-tools\reportgenerator "--reports:tests/TestResults/*-opencover.xml" "--targetdir:tests/TestResults/html"
+reportgenerator "--reports:tests/TestResults/*-opencover.xml" "--targetdir:tests/TestResults/html"
