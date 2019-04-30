d5c9d695caebddb2f846f8ce172a6ef52c774216
Lyrse <lyrise1984@gmail.com>
2019-04-30 12:01:58 +0900

    Add hashcash.linux-x64を追加

--- src/Native/extract.linux-x64.sh
+++ src/Native/extract.linux-x64.sh
@@ -6,3 +6,5 @@ cd "${cwd}"
 cp "./C++/Omnix_Base/Omnix_Base/bin/linux-x64/Omnix_Base.so" "../Omnix.Base/Assemblies/Omnix.Base.linux-x64.so"
 cp "./C++/Omnix_Correction/Omnix_Correction/bin/linux-x64/Omnix_Correction.so" "../Omnix.Correction/Assemblies/Omnix.Correction.linux-x64.so"
 cp "./C++/Omnix_Cryptography/Omnix_Cryptography/bin/linux-x64/Omnix_Cryptography.so" "../Omnix.Cryptography/Assemblies/Omnix.Cryptography.linux-x64.so"
+
+cp "./Rust/hashcash/target/release/hashcash" "../Omnix.Cryptography/Assemblies/hashcash.linux-x64"
