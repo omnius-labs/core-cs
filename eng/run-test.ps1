40e342e37f6bf038237ab347f16ec7df78ff4af0
Lyrse <lyrise1984@gmail.com>
2019-07-06 19:30:43 +0900

    Fix .gitlab-ci.ymlで「dotnet restore --force-evaluate」を行うように修正した。

--- eng/run-test.ps1
+++ eng/run-test.ps1
@@ -5,7 +5,7 @@ dotnet tool update --global dotnet-reportgenerator-globaltool
 
 ForEach ($folder in (Get-ChildItem -Path "tests" -Directory)) 
 {
-    dotnet test $folder.FullName /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="../TestResults/$folder-opencover.xml" /p:Exclude="[xunit*]*%2c[*.Tests]*" -v:n;
+    dotnet test -c Debug $folder.FullName /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="../results/$folder-opencover.xml" /p:Exclude="[xunit*]*%2c[*.Tests]*" -v:n;
 }
 
-reportgenerator.exe "--reports:tests/TestResults/*-opencover.xml" "--targetdir:tests/TestResults/html"
+reportgenerator.exe "--reports:tests/results/*-opencover.xml" "--targetdir:tests/results/html"
