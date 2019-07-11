fccc23afb055f5467dace465b38cca0e22a3ed4d
Lyrse <lyrise1984@gmail.com>
2019-07-11 12:40:29 +0900

    Fix SettingsDatabaseの仕様を変更

--- tests/Omnix.Configuration.Tests/SettingsDatabaseTests.cs
+++ tests/Omnix.Configuration.Tests/SettingsDatabaseTests.cs
@@ -7,9 +7,20 @@ namespace Omnix.Configuration
     public class SettingsDatabaseTests : TestsBase
     {
         [Fact]
-        public void Test1()
+        public void VersionReadWriteTest()
         {
             using var settings = new SettingsDatabase(UnitTestEnvironment.TempDirectoryPath);
+
+            var random = new Random();
+
+            for (int i = 0; i < 32; i++)
+            {
+                uint setVersion = (uint)random.Next();
+
+                Assert.True(settings.TrySetVersion(setVersion));
+                Assert.True(settings.TryGetVersion(out var getVersion));
+                Assert.True(setVersion == getVersion);
+            }
         }
     }
 }
