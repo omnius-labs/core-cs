3445fd218b19afd334e52b9cb48a2acc31e9efc7
Lyrise <lyrise1984@gmail.com>
2019-12-04 06:27:26 +0900

    Fix 不要なテストケースの削除

--- test/Omnius.Core.Collections.Tests/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Assertions", "xUnit2017:Do not use Contains() to check if a value exists in a collection", Justification = "<Pending>", Scope = "member", Target = "~M:Omnius.Core.DataStructures.Tests.SimpleLinkedListTests.SimpleLinkedListTest")]
