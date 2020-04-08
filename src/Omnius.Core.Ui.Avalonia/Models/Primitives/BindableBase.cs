9596a561f0f7e28e50df46e794b9034e31e5cf68
Lyrise <lyrise1984@gmail.com>
2020-04-08 21:34:15 +0900

    Revert "Remove Null For SecureConnection.cs & BindableBase.cs"

--- src/Omnius.Core.Ui.Avalonia/Models/Primitives/BindableBase.cs
+++ src/Omnius.Core.Ui.Avalonia/Models/Primitives/BindableBase.cs
@@ -5,7 +5,7 @@ namespace Omnius.Core.Avalonia.Models.Primitives
 {
     public abstract class BindableBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
