b73d760fef0aa249e00eb25d9a8b03f28d219d15
Lyrise <lyrise1984@gmail.com>
2020-10-02 08:41:24 +0900

    fix

--- src/Omnius.Core.Network/Connections/BaseConnectionOptions.cs
+++ src/Omnius.Core.Network/Connections/BaseConnectionOptions.cs
@@ -2,7 +2,6 @@ namespace Omnius.Core.Network.Connections
 {
     public record BaseConnectionOptions
     {
-        public int MaxSendByteCount = 1024 * 64;
         public int MaxReceiveByteCount = 1024 * 64;
         public IBytesPool? BytesPool;
     }
