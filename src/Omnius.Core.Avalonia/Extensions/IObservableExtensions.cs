e91409224ec8a57704632ad09d5d77a6724ab34c
Lyrise <lyrise1984@gmail.com>
2022-03-29 05:42:25 +0900

    fix

--- src/Omnius.Core.Avalonia/Extensions/IObservableExtensions.cs
+++ src/Omnius.Core.Avalonia/Extensions/IObservableExtensions.cs
@@ -6,35 +6,31 @@ namespace Omnius.Core.Avalonia;
 public static class IObservableExtensions
 {
     // https://stackoverflow.com/questions/67360865/conditional-delaythrottle-operator
-    public static IObservable<T> DelayWhen<T>(this IObservable<T> source, TimeSpan delay, Func<T, bool> condition, IScheduler? scheduler = null)
+    public static IObservable<T> DelayWhen<T>(this IObservable<T> source, TimeSpan delay, Func<T, bool> condition)
     {
-        // Arguments validation omitted
-        scheduler ??= DefaultScheduler.Instance;
-        return source
-            .Select(x => (Item: x, WithDelay: condition(x)))
-            .Publish(published => published.Window(published.Where(e => !e.WithDelay)))
-            .Select(w => Observable.Merge(
-                DelayThrottleSpecial(w.Where(e => e.WithDelay), delay, scheduler),
-                w.Where(e => !e.WithDelay)
-            ))
-            .Switch()
-            .Select(e => e.Item);
+        return DelayWhen(source, delay, condition, CurrentThreadScheduler.Instance);
     }
 
-    /// <summary>
-    /// Time shifts the observable sequence by the specified time duration, ignoring
-    /// elements that are produced while a previous element is scheduled for emission.
-    /// </summary>
-    private static IObservable<T> DelayThrottleSpecial<T>(IObservable<T> source, TimeSpan dueTime, IScheduler scheduler)
+    public static IObservable<T> DelayWhen<T>(this IObservable<T> source, TimeSpan delay, Func<T, bool> condition, IScheduler scheduler)
     {
-        int mutex = 0; // 0: not acquired, 1: acquired
-        return source.SelectMany(x =>
-        {
-            if (Interlocked.CompareExchange(ref mutex, 1, 0) == 0)
-                return Observable.Return(x)
-                    .DelaySubscription(dueTime, scheduler)
-                    .Finally(() => Volatile.Write(ref mutex, 0));
-            return Observable.Empty<T>();
-        });
+        return source
+            .Select(x => (Item: x, WithDelay: condition(x)))
+            .Publish(published => published
+                .Timestamp(scheduler)
+                .Scan((delayOverTime: DateTimeOffset.MinValue, output: Observable.Empty<T>()), (state, t) =>
+                {
+                    if (!t.Value.WithDelay)
+                        //value isn't delayed, current delay status irrelevant, emit immediately, and cancel previous delay.
+                        return (DateTimeOffset.MinValue, Observable.Return(t.Value.Item));
+                    else
+                        if (state.delayOverTime > t.Timestamp)
+                        //value should be delayed, but current delay already in progress. Ignore value.
+                        return (state.delayOverTime, Observable.Empty<T>());
+                    else
+                        //value should be delayed, no delay in progress. Set delay state, and return delayed observable.
+                        return (t.Timestamp + delay, Observable.Return(t.Value.Item).Delay(delay, scheduler).TakeUntil(published.Where(t2 => !t2.WithDelay)));
+                })
+            )
+            .SelectMany(t => t.output);
     }
 }
