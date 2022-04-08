377aa3fc40d23ad4adc8e9137d8e159d35bfa918
Lyrise <lyrise1984@gmail.com>
2022-04-09 01:29:36 +0900

    fix

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ gen-code:
 	bash ./eng/gen-code.sh
 
 test:
+	dotnet format --verify-no-changes
 	dotnet test
 
 build:
--- src/Omnius.Core.Net.Connections/Multiplexer/V1/Internal/StreamConnection.ConnectionReceiver.cs
+++ src/Omnius.Core.Net.Connections/Multiplexer/V1/Internal/StreamConnection.ConnectionReceiver.cs
@@ -5,10 +5,6 @@ namespace Omnius.Core.Net.Connections.Multiplexer.V1.Internal;
 
 internal partial class StreamConnection
 {
-    public StreamConnection()
-    {
-    }
-
     public sealed class ConnectionReceiver : DisposableBase, IConnectionReceiver
     {
         private readonly IMessagePipeReader<ArraySegment<byte>> _dataReader;
