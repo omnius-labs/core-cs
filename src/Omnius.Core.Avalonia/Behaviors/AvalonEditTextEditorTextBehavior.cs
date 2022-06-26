636d10cf68568bc6c4ad5994d64b1d81cf9b8c68
Lyrise <lyrise1984@gmail.com>
2022-06-26 10:41:40 +0900

    fix

---
+++ src/Omnius.Core.Avalonia/Behaviors/AvalonEditTextEditorTextBehavior.cs
@@ -0,0 +1,60 @@
+using Avalonia;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit;
+
+namespace Omnius.Core.Avalonia.Behaviors;
+
+public class AvalonEditTextEditorTextBehavior : Behavior<TextEditor>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        var editor = this.AssociatedObject as TextEditor;
+        if (editor == null) return;
+
+        editor.TextChanged += this.TextChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        var editor = this.AssociatedObject as TextEditor;
+        if (editor == null) return;
+
+        editor.TextChanged -= this.TextChanged;
+
+        base.OnDetaching();
+    }
+
+    public static readonly DirectProperty<AvalonEditTextEditorTextBehavior, string> TextProperty =
+        AvaloniaProperty.RegisterDirect<AvalonEditTextEditorTextBehavior, string>(
+            nameof(Text),
+            o => o.Text,
+            (o, v) => o.Text = v);
+
+    private string _text = string.Empty;
+
+    public string Text
+    {
+        get { return _text; }
+        set
+        {
+            SetAndRaise(TextProperty, ref _text, value);
+
+            var editor = this.AssociatedObject as TextEditor;
+            if (editor == null) return;
+            if (editor.Text == value) return;
+
+            editor.Text = value;
+        }
+    }
+
+    private void TextChanged(object? sender, EventArgs e)
+    {
+        var editor = this.AssociatedObject as TextEditor;
+        if (editor == null) return;
+        if (editor.Text == this.Text) return;
+
+        this.Text = editor.Text;
+    }
+}
