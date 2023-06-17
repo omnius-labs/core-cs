a9b2a33507c7ff445b6406dc00e26c23f2839ac7
Lyrise <lyrise1984@gmail.com>
2023-06-17 21:54:40 +0900

    fix

--- src/Omnius.Core.UnitTestToolkit/StableTestSkipAttribute.cs
+++ src/Omnius.Core.UnitTestToolkit/StableTestSkipAttribute.cs
@@ -5,7 +5,8 @@ namespace Omnius.Core.UnitTestToolkit;
 public class StableTestSkipAttribute : FactAttribute
 {
 #if STABLE_TEST
-    public StableTestSkipAttribute() {
+    public StableTestSkipAttribute()
+    {
         Skip = "stable test";
     }
 #endif
